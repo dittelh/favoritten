aa2962aea0ba02731078344cdd3dbe835815fe32
dittelh <dittehansen143@gmail.com>
2021-11-30 22:49:42 +0100

    added js function
    added for highlighting

--- frokost/script.js
+++ frokost/script.js
@@ -5,37 +5,60 @@ var delivery = 20;
 var price = 0;
 var totalPrice = 0;
 
-var priceElement = document.getElementById('price');
-var totalPriceElement = document.getElementById('totalPrice')
-var mealsElement = document.getElementById('output');
-var pId = document.getElementById('pId');
+var priceElement = document.getElementById("price");
+var totalPriceElement = document.getElementById("totalPrice");
+var mealsElement = document.getElementById("output");
+var pId = document.getElementById("pId");
 
-
-function categoryValue(e){
-
-    categoryPrice = e.target.value;
-    setPrice();
+function categoryValue(e) {
+  categoryPrice = e.target.value;
+  setPrice();
 }
 
-function personsValue(e){
-    persons = e.target.value;
-    setPrice();
+function personsValue(e) {
+  persons = e.target.value;
+  setPrice();
 }
 
-function mealsValue(){
-    meals = document.getElementById("slider").value;
-    mealsElement.innerHTML = meals;
+function mealsValue() {
+  meals = document.getElementById("slider").value;
+  mealsElement.innerHTML = meals;
 
-    setPrice();
+  setPrice();
 }
 
-function setPrice(){
-    price = persons * (meals * categoryPrice);
-    totalPrice = price + delivery
+function setPrice() {
+  price = persons * (meals * categoryPrice);
+  totalPrice = price + delivery;
 
-    priceElement.innerHTML = price;
-    totalPriceElement.innerHTML = totalPrice
+  priceElement.innerHTML = price;
+  totalPriceElement.innerHTML = totalPrice;
 
-    pId.innerHTML = meals + " måltider for " + persons + " personer per uge";
+  pId.innerHTML = meals + " måltider for " + persons + " personer per uge";
 }
 setPrice();
+
+function highlightedFoodChoice(id) {
+    // Remove all classes
+  document.getElementById("vegetarisk").classList.remove("highlightedChoice");
+  document.getElementById("fisk").classList.remove("highlightedChoice");
+  document.getElementById("kaloriefattigt").classList.remove("highlightedChoice");
+  document.getElementById("boernevenligt").classList.remove("highlightedChoice");
+  document.getElementById("glutenfrit").classList.remove("highlightedChoice");
+  document.getElementById("proteinrigt").classList.remove("highlightedChoice");
+
+    // Set highlight class on the clicked option
+  document.getElementById(id).classList.add("highlightedChoice");
+}
+
+
+function highlightedPersonChoice(id) {
+    // Remove all classes
+  document.getElementById("1").classList.remove("highlightedChoice");
+  document.getElementById("2").classList.remove("highlightedChoice");
+  document.getElementById("3").classList.remove("highlightedChoice");
+  document.getElementById("4").classList.remove("highlightedChoice");
+
+    // Set highlight class on the clicked option
+  document.getElementById(id).classList.add("highlightedChoice");
+}
